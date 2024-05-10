af2333c47fb4587c42e43a2e09df2302b7a8f0cf
Julien <julien@themosis.com>
2024-05-10 18:09:16 +0200

    Add yabairc configuration.

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 session=$(find ~ /Volumes/Materia/dev -mindepth 1 -maxdepth 1 -type d | fzf)
 session_name=$(basename "$session" | tr . _)
 
-if ! tmux has-session -t "$session_name"; then
+if ! tmux has-session -t "$session_name" 2> /dev/null; then
     tmux new-session -s "$session_name" -c "$session" -d
 fi
 
