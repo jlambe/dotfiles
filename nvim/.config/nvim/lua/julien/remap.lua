2ad5ed5514aa09d64b5ffe6fc981e5bf1a3d4e14
Julien Lambé <julien@themosis.com>
2025-05-11 14:18:36 +0200

    Set nvim leader key to comma character.

--- nvim/.config/nvim/lua/julien/remap.lua
+++ nvim/.config/nvim/lua/julien/remap.lua
@@ -1,6 +1,6 @@
 -- Remap leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Open Netrw
 -- vim.keymap.set('n', '<C-p>', vim.cmd.Ex)
