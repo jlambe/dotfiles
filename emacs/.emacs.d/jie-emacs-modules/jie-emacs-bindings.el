989b09ed12e8cb894a4d225aab800f732bb78f25
Julien Lambé <julien@themosis.com>
2025-05-21 18:16:18 +0200

    Update keybindings.

--- emacs/.emacs.d/jie-emacs-modules/jie-emacs-bindings.el
+++ emacs/.emacs.d/jie-emacs-modules/jie-emacs-bindings.el
@@ -1,18 +1,27 @@
-;;; C-c i move the point at the beginning of line text
-(keymap-global-set "C-c i" 'beginning-of-line-text)
+;;; C-A move the point at the beginning of line text
+(keymap-global-set "C-S-a" 'beginning-of-line-text)
 
-;;; C-E keybinding to scroll down by line
-;;; C-Y keybinding to scroll up by line
-(keymap-global-set "C-S-E" 'scroll-down-line)
-(keymap-global-set "C-S-Y" 'scroll-up-line)
+;;; C-E keybinding to scroll up by line
+;;; C-Y keybinding to scroll down by line
+(keymap-global-set "C-S-Y" 'scroll-down-line)
+(keymap-global-set "C-S-E" 'scroll-up-line)
 
-;;; C-c l Move to window right
-;;; C-c h Move to window left
-;;; C-c j Move to window below
-;;; C-c k Move to window up
-(keymap-global-set "C-c l" 'windmove-right)
-(keymap-global-set "C-c h" 'windmove-left)
-(keymap-global-set "C-c j" 'windmove-down)
-(keymap-global-set "C-c k" 'windmove-up)
+;;; C-L Move to window right
+;;; C-H Move to window left
+;;; C-J Move to window below
+;;; C-K Move to window up
+(keymap-global-set "C-S-l" 'windmove-right)
+(keymap-global-set "C-S-h" 'windmove-left)
+(keymap-global-set "C-S-j" 'windmove-down)
+(keymap-global-set "C-S-k" 'windmove-up)
+
+;;; LSP related keybindings
+(keymap-global-set "C-, C-," 'xref-go-back)
+(keymap-global-set "C-, C-d" 'xref-find-definitions)
+(keymap-global-set "C-, C-r" 'xref-find-references)
+
+(keymap-global-set "C-, C-S-d" 'eglot-find-declaration)
+(keymap-global-set "<f7>" 'eglot-format-buffer)
+(keymap-global-set "<f6>" 'eglot-rename)
 
 (provide 'jie-emacs-bindings)
