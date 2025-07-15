a904b275db330346f4250f82c30ec10e813b6614
Julien Lambé <julien@themosis.com>
2025-07-15 22:27:43 +0200

    Update emacs configuration.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -46,3 +46,18 @@
 (require 'jie-emacs-vc)
 (require 'jie-emacs-vterm)
 (require 'jie-emacs-bindings)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cape corfu-terminal doom-themes ef-themes exec-path-from-shell magit
+	  marginalia markdown-mode nerd-icons orderless php-mode
+	  quelpa vertico vterm web-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
