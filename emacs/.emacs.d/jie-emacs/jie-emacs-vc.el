b28955f7469da44d1c3e4317560d4519bc80ba42
Julien Lambé <julien@themosis.com>
2025-09-23 12:22:18 +0200

    Add vc gutter highlights support.

--- emacs/.emacs.d/jie-emacs/jie-emacs-vc.el
+++ emacs/.emacs.d/jie-emacs/jie-emacs-vc.el
@@ -1,6 +1,14 @@
 ;;; -*- lexical-binding: t -*-
-
 (use-package magit
   :ensure t)
+
+;; Add version control gutter highlights.
+(use-package diff-hl
+  :if (display-graphic-p)
+  :ensure t
+  :config
+  (global-diff-hl-mode)
+  :hook
+  (magit-post-refresh . diff-hl-magit-post-refresh))
 
 (provide 'jie-emacs-vc)
